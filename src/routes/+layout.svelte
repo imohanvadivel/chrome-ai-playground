6390666de0a52223108946cda6acf50873f1fe14
mohan <mohan@mohanvadivel.com>
2025-09-16 09:25:18 +0530

    updated the meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,6 @@
     import "$lib/styles/global.css";
 </script>
 
-<svelte:head>
-    <title>AI Playground</title>
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-</svelte:head>
-
 <div class="layout-cnt">
     <slot />
 </div>
